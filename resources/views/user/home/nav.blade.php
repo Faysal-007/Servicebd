dd41ab1dfd990bb61ba619c6ce0a2e9f943f681a
Faysal-007 <faysal.bas09@gmail.com>
2024-05-23 14:53:08 +0600

    logo test chnage

--- resources/views/user/home/nav.blade.php
+++ resources/views/user/home/nav.blade.php
@@ -1,7 +1,7 @@
 <nav id="gtco-header-navbar" class="navbar navbar-expand-lg py-4">
     <div class="container">
         <a class="navbar-brand d-flex align-items-center" href="/">
-            Service Bangladesh
+            BD
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-nav-header" aria-controls="navbar-nav-header" aria-expanded="false" aria-label="Toggle navigation">
             <span class="lnr lnr-menu"></span>
