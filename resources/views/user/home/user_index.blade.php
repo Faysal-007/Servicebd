8b8bd47cb33e3024fb1daea25f944a90fa7708cd
Faysal-007 <faysal.bas09@gmail.com>
2024-05-30 11:03:00 +0600

    add yielded contents

--- resources/views/user/home/user_index.blade.php
+++ resources/views/user/home/user_index.blade.php
@@ -5,49 +5,30 @@ Service BD
 @endsection
 
 @section('nav')
-@include('user.home.nav')
+@include('user.home.top_bar')
 @endsection
 
 @section('carousel')
-@include('user.home.carousel')
+@include('user.home.navbar')
 @endsection
 
 @section('who_we_are')
-@include('user.home.who_we_are')
+@include('user.home.search')
 @endsection
 
 @section('counter')
-@include('user.home.counter')
+@include('user.home.facts')
 @endsection
 
 @section('feature')
-@include('user.home.feature')
+@include('user.home.about')
 @endsection
 
 @section('social')
-@include('user.home.social')
+@include('user.home.features')
 @endsection
 
 @section('latest_news')
-@include('user.home.latest_news')
+@include('user.home.services')
 @endsection
-
-
-
-@section('pricing')
-@include('user.home.pricing')
-@endsection
-
-@section('clients')
-@include('user.home.clients')
-@endsection
-
-@section('footer')
-@include('user.home.footer')
-@endsection
-
-
-
-
-
 
